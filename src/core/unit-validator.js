6d3facc6008357bb89f98cd6e05fa7295e7b689d
JanuitaM <januita.marpaung@moonlay.com>
2016-09-16 11:16:24 +0700

    make  validator unit and ategory

---
+++ src/core/unit-validator.js
@@ -0,0 +1,19 @@
+require("should"); 
+
+module.exports = function (data) {
+    data.should.not.equal(null);
+    data.should.instanceOf(Object);
+
+    data.should.have.property('code');
+    data.code.should.be.String();
+
+    data.should.have.property('division');
+    data.division.should.be.String();
+
+    data.should.have.property('subdivision');
+    data.subdivision.should.be.String();
+
+    data.should.have.property('description');
+    data.description.should.be.String(); 
+    
+}
