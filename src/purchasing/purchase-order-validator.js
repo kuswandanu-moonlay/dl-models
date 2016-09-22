10a97135ad25f77cd88331a99bdaaa04412c5bd6
Indri.Hutabalian <indri.hutabalian@moonlay.com>
2016-09-22 11:21:26 +0700

    add field conversion

--- src/purchasing/purchase-order-validator.js
+++ src/purchasing/purchase-order-validator.js
@@ -94,6 +94,9 @@ module.exports = function (data) {
     data.should.have.property('actualDeliveryDate');
     data.actualDeliveryDate.should.instanceof(Date);
 
+    data.should.have.property('conversion');
+    data.conversion.should.instanceOf(String);
+    
     data.should.have.property('remark');
     data.remark.should.instanceOf(String);
     
