ca3db299fb98c1feb4a110052932809dc80afadc
JanuitaM <januita.marpaung@moonlay.com>
2016-10-25 10:12:00 +0700

    add some field in model unit-payment-order (#111)
    * add model unit-payment-order
    
    * delete field in unit-payment-order-item-validator
    
    * add this.copy(source) in unit-payment-order-item
    
    * add field unit-payment-order

--- src/purchasing/unit-payment-order-validator.js
+++ src/purchasing/unit-payment-order-validator.js
@@ -1,19 +1,37 @@
 require("should");
 var validateSupplier = require('../master/supplier-validator');
 var validateUnit = require('../master/unit-validator');
-
+var validateCurrency = require('../master/currency-validator');
 var validateUnitPaymentOrderItem = require('./unit-payment-order-item-validator');
- 
+var validateVat=require('../master/vat-validator');
+
 module.exports = function (data) {
     data.should.have.property('no');
     data.no.should.instanceOf(String);
-    
+
     data.should.have.property('unitId');
     data.unitId.should.instanceof(Object);
 
     data.should.have.property('unit');
     data.unit.should.instanceof(Object);
     validateUnit(data.unit);
+
+    data.should.have.property('categoryId');
+    data.categoryId.should.instanceof(Object);
+
+    data.should.have.property('category');
+    data.category.should.instanceof(Object);
+
+    data.should.have.property('currency');
+    data.supplier.should.instanceof(Object);
+    validateCurrency(data.currency);
+
+    data.should.have.property('vat');
+    data.vat.should.instanceof(Object);
+    validateVat(data.vat);
+
+    data.should.have.property('paymentMethod');
+    data.paymentMethod.should.instanceOf(String);
 
     data.should.have.property('date');
     data.date.should.instanceOf(Date);
@@ -24,7 +42,7 @@ module.exports = function (data) {
     data.should.have.property('supplier');
     data.supplier.should.instanceof(Object);
     validateSupplier(data.supplier);
-    
+
     data.should.have.property('invoceNo');
     data.invoceNo.should.instanceOf(String);
 
@@ -41,10 +59,10 @@ module.exports = function (data) {
     data.vatNo.should.instanceOf(String);
 
     data.should.have.property('vatDate');
-    data.vatDate.should.instanceOf(Date);  
+    data.vatDate.should.instanceOf(Date);
 
     data.should.have.property('dueDate');
-    data.dueDate.should.instanceOf(Date);  
+    data.dueDate.should.instanceOf(Date);
 
     data.should.have.property('vatRate');
     data.vatRate.should.instanceOf(Number);
