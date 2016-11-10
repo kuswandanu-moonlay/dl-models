bb95ec71bdfe58f9bb44870eed7c6abe4199c252
Indri.Hutabalian <indri.hutabalian@moonlay.com>
2016-11-10 13:56:39 +0700

    add realizationQuantity

--- src/purchasing/delivery-order-item-fulfillment.js
+++ src/purchasing/delivery-order-item-fulfillment.js
@@ -14,6 +14,7 @@ module.exports = class DeliveryOrderItemFulfillment extends BaseModel {
         this.purchaseOrderQuantity = 0;
         this.purchaseOrderUom = new uom();
         this.deliveredQuantity = 0;
+        this.realizationQuantity = 0;
         this.remark = '';
 
         this.copy(source);
