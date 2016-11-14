776d6199695188fb33e3f5337a3fee4dfc85dcdd
SirDimas <dimassixer@gmail.com>
2016-11-14 14:13:49 +0700

    Models Uster Classification

---
+++ src/master/uster-classification.js
@@ -0,0 +1,21 @@
+'use strict'
+
+var BaseModel = require('capital-models').BaseModel;
+var Product = require('./product');
+
+module.exports = class UsterClassification extends BaseModel {
+    constructor(source) {
+        super('uster-classification', '1.0.0');
+
+        // Define properties.
+        this.productId = {};  
+        this.thread = new Product();
+        this.thin = 0;
+        this.thick = 0;
+        this.neps = 0;
+        this.ipi = 0
+        this.grade = '';
+
+        this.copy(source);
+    }
+}
