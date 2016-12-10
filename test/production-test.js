33d293f87bde846a366d93b6d0e7352550e63201
achensetyawati <achenzetya@gmail.com>
2016-12-10 12:05:13 +0700

    adding test production order

--- test/production-test.js
+++ test/production-test.js
@@ -4,3 +4,13 @@ it("#01. Quality Hasil Produksi Spinning should valid", function () {
     var WindingQualitySampling = require('../src/production/spinning/winding/winding-quality-sampling');
     validate.spinning.winding.windingQualitySampling(new WindingQualitySampling());
 });
+
+it("#02. Output Hasil Produksi Spinning should valid", function () {
+    var windingProductionOutput = require('../src/production/spinning/winding/winding-production-output');
+    validate.spinning.winding.windingProductionOutput(new WindingProductionOutput());
+});
+
+it("#03. Production Order should valid", function () {
+    var productionOrder = require('../src/production/production-order');
+    validate.spinning.winding.productionOrder(new ProductionOrder());
+});
