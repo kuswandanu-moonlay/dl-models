a8b465c8de818df193ad35e53c31da7f8de3cf1f
indriHutabalian <indri.hutabalian@moonlay.com>
2016-12-22 10:25:44 +0700

    fix label enum PO

--- src/purchasing/enum/purchase-order-status-enum.js
+++ src/purchasing/enum/purchase-order-status-enum.js
@@ -25,7 +25,7 @@ module.exports =
         'ARRIVING': {
             'name': 'ARRIVING',
             'value': 4,
-            'label': 'Barang sudah datang'
+            'label': 'Barang sudah datang Parsial'
         },
         'ARRIVED': {
             'name': 'ARRIVED',
@@ -35,7 +35,7 @@ module.exports =
         'RECEIVING': {
             'name': 'RECEIVING',
             'value': 6,
-            'label': 'Barang sudah diterima Unit'
+            'label': 'Barang sudah diterima Unit Parsial'
         },
         'RECEIVED': {
             'name': 'RECEIVED',
