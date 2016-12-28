0a668c1b8b1d273cf9ee0f1a7636a4de00fe277a
achensetyawati <achenzetya@gmail.com>
2016-12-28 11:01:44 +0700

    updating master Buyer

--- src/master/buyer-validator.js
+++ src/master/buyer-validator.js
@@ -21,4 +21,7 @@ module.exports = function (data) {
 
     data.should.have.property('tempo');
     data.tempo.should.instanceOf(String);
+
+    data.should.have.property('type');
+    data.type.should.instanceOf(String);
 };
