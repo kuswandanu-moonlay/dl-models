0f7aa1e6717290cd4edf0ba1b2f9cdf615cbf85d
achensetyawati <achenzetya@gmail.com>
2016-12-28 10:54:32 +0700

    adding process type master

---
+++ src/master/process-type-validator.js
@@ -0,0 +1,24 @@
+require("should"); 
+var validateOrderType=require('./order-type-validator')
+
+module.exports = function (data) {
+    data.should.not.equal(null);
+    data.should.instanceOf(Object);
+
+    data.should.have.property('code');
+    data.code.should.be.String();
+
+    data.should.have.property('orderTypeId');
+    data.orderTypeId.should.be.Object();
+    
+    data.should.have.property('orderType');
+    data.orderType.should.instanceof(Object);
+    validateOrderType(data.orderType);
+
+    data.should.have.property('name');
+    data.name.should.be.String();
+
+    data.should.have.property('remark');
+    data.remark.should.be.String(); 
+    
+};
