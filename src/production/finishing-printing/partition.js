cb2e5bfa2d3f1d935ebc6f6446e830a95e691847
achensetyawati <achenzetya@gmail.com>
2017-01-13 16:38:07 +0700

    adding partition

--- src/production/finishing-printing/partition.js
+++ src/production/finishing-printing/partition.js
@@ -1,12 +1,27 @@
 'use strict';
-var BaseModel = require('model-toolkit').BaseModel; 
+
+var BaseModel = require('model-toolkit').BaseModel;
+var Step = require("../../master/step");
+var Machine = require("../../master/machine");
 
 module.exports = class Partition extends BaseModel {
     constructor(source) {
         super('partition', '1.0.0');
-        this.no = 0;
-        this.total = 0;
-        this.
+
+        // Define properties.  
+        this.no = '';
+        this.shift = '';
+        this.stepId = {};
+        this.step = new Step();
+        this.steps = [];
+        this.machineId = {};
+        this.machine = new Machine();
+        this.dateInput = new Date();
+        this.input = 0;
+        this.dateOutput = new Date();
+        this.goodOutput = 0;
+        this.badOutput = 0;
+        this.badOutputDescription = '';
 
         this.copy(source);
     }
