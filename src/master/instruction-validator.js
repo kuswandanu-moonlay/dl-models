82dc0985efc720b065e49ade23a5000cbce0151c
achensetyawati <achenzetya@gmail.com>
2017-01-13 16:36:42 +0700

    updating instruction

--- src/master/instruction-validator.js
+++ src/master/instruction-validator.js
@@ -1,17 +1,35 @@
 require("should");
+var validateOrderType=require('./order-type-validator');
+var validateProduct = require('./product-validator');
+var validateColorType=require('./color-type-validator');
 
 module.exports = function (data) {
     data.should.not.equal(null);
     data.should.instanceOf(Object);
+
+    data.should.have.property('materialId');
+    data.materialId.should.instanceof(Object);
+
+    data.should.have.property('material');
+    data.material.should.instanceof(Object);
+    validateProduct(data.material);
     
-    data.should.have.property('processType');
-    data.processType.should.be.String();
+    data.should.have.property('orderTypeId');
+    data.orderTypeId.should.be.Object();
+    
+    data.should.have.property('orderType');
+    data.orderType.should.instanceof(Object);
+    validateOrderType(data.orderType);
+    
+    data.should.have.property('colorTypeId');
+    data.colorTypeId.should.be.Object();
+    
+    data.should.have.property('colorType');
+    data.colorType.should.instanceof(Object);
+    validateColorType(data.colorType);
 
     data.should.have.property('construction');
     data.construction.should.be.String();
-
-    data.should.have.property('material');
-    data.material.should.be.String();
 
     data.should.have.property('steps');
     data.steps.should.instanceOf(Array);
--- src/master/instruction.js
+++ src/master/instruction.js
@@ -1,13 +1,24 @@
 'use strict';
 
 var BaseModel = require('model-toolkit').BaseModel; 
+var Product = require('./product');
+var OrderType = require('./order-type');
+var ColorType = require('./color-type');
 
 module.exports = class instruction extends BaseModel{
     constructor(source){
         super('instruction', '1.0.0');
-        this.material = '';
+
+        this.materialId= {};
+        this.material = new Product();
+
+        this.orderTypeId={};
+        this.orderType=new OrderType();
+
+        this.colorTypeId={};
+        this.colorType=new ColorType();
+
         this.construction = '';
-        this.processType = '';
         this.steps=[];
         
         this.copy(source);
