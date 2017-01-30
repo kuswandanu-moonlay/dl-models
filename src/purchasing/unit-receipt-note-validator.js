98dddc3c1eb472c5dd73262ba74d68d7bd41ee5a
indriHutabalian <indri.hutabalian@moonlay.com>
2017-01-30 18:11:41 +0700

    fix unit payment order

--- src/purchasing/unit-receipt-note-validator.js
+++ src/purchasing/unit-receipt-note-validator.js
@@ -35,8 +35,8 @@ module.exports = function (data) {
     data.should.have.property('remark');
     data.remark.should.instanceOf(String);
 
-    data.should.have.property('isPaid');
-    data.isPaid.should.instanceOf(Boolean);
+    // data.should.have.property('isPaid');
+    // data.isPaid.should.instanceOf(Boolean);
 
     data.should.have.property('items');
     data.items.should.instanceOf(Array);
