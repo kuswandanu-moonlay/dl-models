d0893c455ccdd22b4caba6439ad4771ad635b88f
SirDimas <dimassixer@gmail.com>
2017-02-10 14:58:12 +0700

    update

--- test/sales-test.js
+++ test/sales-test.js
@@ -4,3 +4,8 @@ it("#01. Sales Contract should valid", function () {
     var salesContract = require('../src/sales/sales-contract');
     validate.salesContract(new salesContract());
 });
+
+it("#02. Production Order should valid", function () {
+    var productionOrder = require('../src/sales/production-order');
+    validate.productionOrder(new productionOrder());
+});
