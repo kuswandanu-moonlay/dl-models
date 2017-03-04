b28829d2bc3c9a3a4411f933a461c4a759484636
achensetyawati <achenzetya@gmail.com>
2017-03-04 12:30:59 +0700

    spinning sales contract

---
+++ src/sales/spinning-sales-contract-validator.js
@@ -0,0 +1,75 @@
+require("should");
+
+var validateBuyer = require('../master/buyer-validator');
+var validateUom = require('../master/uom-validator');
+var validateAccount= require('../master/account-bank-validator');
+var validateQuality = require('../master/quality-validator');
+var validateComodity = require('../master/comodity-validator');
+
+module.exports = function (data) {
+    data.should.not.equal(null);
+    data.should.instanceOf(Object);
+
+    data.should.have.property('salesContractNo');
+    data.salesContractNo.should.be.String();
+
+    data.should.have.property('buyerId');
+    data.buyerId.should.instanceof(Object);
+
+    data.should.have.property('buyer');
+    data.buyer.should.instanceof(Object);
+    validateBuyer(data.buyer);
+
+    data.should.have.property('uomId');
+    data.uomId.should.instanceof(Object);
+
+    data.should.have.property('uom');
+    data.uom.should.instanceof(Object);
+    validateUom(data.uom);
+
+    data.should.have.property('shippingQuantityTolerance');
+    data.spelling.should.instanceOf(Number);
+
+    data.should.have.property('qualityId');
+    data.qualityId.should.instanceof(Object);
+
+    data.should.have.property('quality');
+    data.quality.should.instanceof(Object);
+    validateQuality(data.quality);
+
+    data.should.have.property('comodityId');
+    data.comodityId.should.instanceof(Object);
+
+    data.should.have.property('comodity');
+    data.comodity.should.instanceof(Object);
+    validateComodity(data.comodity);
+
+    data.should.have.property('rollLength');
+    data.rollLength.should.be.String();
+
+    data.should.have.property('paymentMethod');
+    data.paymentMethod.should.be.String();
+
+    data.should.have.property('paymentRequirement');
+    data.paymentRequirement.should.be.String();
+
+    data.should.have.property('incomeTax');
+    data.incomeTax.should.be.String();;
+
+    data.should.have.property('accountBankId');
+    data.accountBankId.should.instanceof(Object);
+
+    data.should.have.property('accountBank');
+    data.accountBank.should.instanceof(Object);
+    validateAccount(data.accountBank);
+
+    data.should.have.property('deliveredTo');
+    data.deliveredTo.should.be.String();
+
+    data.should.have.property('condition');
+    data.condition.should.be.String();
+
+    data.should.have.property('deliverySchedule');
+    data.deliverySchedule.should.instanceof(Date);
+
+};
---
+++ src/sales/spinning-sales-contract.js
@@ -0,0 +1,55 @@
+'use strict';
+var BaseModel = require('model-toolkit').BaseModel;
+var Buyer = require('../master/buyer');
+var uom = require('../master/uom');
+var AccountBank = require('../master/account-bank');
+var Comodity = require('../master/comodity');
+var Quality = require('../master/quality');
+
+
+module.exports = class SpinningSalesContract extends BaseModel {
+    constructor(source) {
+        super('spinning-sales-contract', '1.0.0');
+
+        this.salesContractNo='';
+
+        this.buyerId={};
+        this.buyer= new Buyer();
+
+        this.dispositionNumber='';
+
+        this.uomId={};
+        this.uom=new uom();
+
+        this.orderQuantity=0;
+        this.shippingQuantityTolerance=0;
+
+        this.comodityId={};
+        this.comodity=new Comodity();
+
+        this.comodityDescription='';
+
+        this.qualityId={};
+        this.quality=new Quality();
+
+        this.rollLength='';
+        this.incomeTax = '';
+        this.paymentMethod = '';
+        this.paymentRequirement='';
+
+        this.accountBankId={};
+        this.accountBank=new AccountBank();
+
+        this.transportFee='';
+        this.deliveredTo='';
+        this.packing='';
+        this.agent='';
+        this.comission=0;
+        this.deliverySchedule=new Date();
+        this.condition='';
+        this.attachment='';
+        this.remark='';
+
+        this.copy(source);
+    }
+};
