1ad6a57740585782990fda9424fbf5756bc1b32f
achensetyawati <achenzetya@gmail.com>
2017-03-04 12:30:47 +0700

    finishing-printing sales contract

---
+++ src/sales/finishing-printing-sales-contract.js
@@ -0,0 +1,81 @@
+'use strict';
+var BaseModel = require('model-toolkit').BaseModel;
+var ProductionOrder=require('./production-order');
+var Buyer = require('../master/buyer');
+var Product = require('../master/product');
+var OrderType = require('../master/order-type');
+var ProcessType = require('../master/process-type');
+var uom = require('../master/uom');
+var AccountBank = require('../master/account-bank');
+var SalesContractDetail=require('./finishing-printing-sales-contract-detail');
+var MaterialConstruction=require('../master/material-construction');
+var SalesContractDetail=require('./finishing-printing-sales-contract-detail');
+var Comodity = require('../master/comodity');
+var Quality = require('../master/quality');
+
+
+module.exports = class FinishingPrintingSalesContract extends BaseModel {
+    constructor(source) {
+        super('finishing-printing-sales-contract', '1.0.0');
+
+        this.salesContractNo='';
+
+        this.buyerId={};
+        this.buyer= new Buyer();
+
+        this.dispositionNumber='';
+
+        this.processTypeId={};
+        this.processType=new ProcessType();
+
+        this.orderTypeId={};
+        this.orderType=new OrderType();
+
+        this.materialId={};
+        this.material=new Product();
+
+        this.uomId={};
+        this.uom=new uom();
+
+        this.materialConstructionId={};
+        this.materialConstruction=new MaterialConstruction();
+        
+        this.yarnMaterialId={};
+        this.yarnMaterial=new YarnMaterial();
+
+        this.materialWidth='';
+        this.orderQuantity=0;
+        this.shippingQuantityTolerance=0;
+
+        this.comodityId={};
+        this.comodity=new Comodity();
+
+        this.comodityDescription='';
+
+        this.qualityId={};
+        this.quality=new Quality();
+
+        this.rollLength='';
+        this.useIncomeTax = false;
+        this.paymentMethod = '';
+        this.paymentRequirement='';
+
+        this.accountBankId={};
+        this.accountBank=new AccountBank();
+
+        this.transportFee='';
+        this.deliveredTo='';
+        this.packing='';
+        this.agent='';
+        this.comission=0;
+        this.deliverySchedule=new Date();
+        this.condition='';
+        this.attachment='';
+        this.remark='';
+
+        this.details=[];
+        this.details = (this.details || []).map(detail => new SalesContractDetail(detail));
+
+        this.copy(source);
+    }
+};
