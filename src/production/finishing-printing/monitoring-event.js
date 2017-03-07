dd25e149b394db4a1c22bb644435536cf6200c7c
dhifa.irawan <dhifa.irawan@moonlay.com>
2017-03-07 17:51:36 +0700

    fix dateTimeEnd not required

--- src/production/finishing-printing/monitoring-event.js
+++ src/production/finishing-printing/monitoring-event.js
@@ -26,7 +26,7 @@ module.exports = class MonitoringEvent extends BaseModel {
         this.remark = '';
 
         this.copy(source);
-        this.dateEnd = source.dateEnd;
-        this.timeInMillisEnd = source.timeInMillisEnd;
+        this.dateEnd = source ? source.dateEnd : null;
+        this.timeInMillisEnd = source ? source.timeInMillisEnd : null;
     }
 };
