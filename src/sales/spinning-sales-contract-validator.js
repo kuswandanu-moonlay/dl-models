b19d1f65056b702b15fdb322e27e72ba8245b135
Leslie Aula <cristiano.ulo@gmail.com>
2017-03-14 18:38:38 +0700

    update spinning sales contract

--- src/sales/spinning-sales-contract-validator.js
+++ src/sales/spinning-sales-contract-validator.js
@@ -55,6 +55,9 @@ module.exports = function (data) {
     data.termOfPayment.should.instanceof(Object);
     validateTermOfPayment(data.termOfPayment);
 
+    data.should.have.property('termOfShipment');
+    data.termOfShipment.should.be.String();
+
 
     data.should.have.property('incomeTax');
     data.incomeTax.should.be.String();
@@ -71,6 +74,9 @@ module.exports = function (data) {
 
     data.should.have.property('condition');
     data.condition.should.be.String();
+
+    data.should.have.property('attachment');
+    data.termOfShipment.should.be.String();
 
     data.should.have.property('deliverySchedule');
     data.deliverySchedule.should.instanceof(Date);
