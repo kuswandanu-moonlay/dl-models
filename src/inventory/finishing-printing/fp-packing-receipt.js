256c71e76119f9f322617c21f2240140da209596
Tris Setiawan <tris.setiawan@yahoo.com>
2017-04-13 10:09:00 +0000

    update inventory/finishing-printing models

---
+++ src/inventory/finishing-printing/fp-packing-receipt.js
@@ -0,0 +1,21 @@
+'use strict';
+
+var BaseModel = require('model-toolkit').BaseModel;
+var Uom = require("../../master/uom");
+
+module.exports = class FPPackingReceipt extends BaseModel {
+    constructor(source) {
+        super('fp-packing-receipt', '1.0.0');
+
+        // Define properties.
+        this.code = ''; 
+        this.date = new Date();
+        this.packingId = {};
+        this.packingCode = "";
+        this.accepted = false;
+        this.declined = false;
+        this.remark = "";
+        
+        this.copy(source);
+    }
+};
