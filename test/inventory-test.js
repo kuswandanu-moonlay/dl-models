8497c1ec923a22ca12c866b3419794eafb50b49a
achensetyawati <achenzetya@gmail.com>
2017-07-31 16:30:53 +0700

    fix test retur to qc

--- test/inventory-test.js
+++ test/inventory-test.js
@@ -37,5 +37,5 @@ it("#07. finishing printing shipment document should valid", function () {
 
 it("#08. finishing-printing/retur-to-qc should valid", function () {
     var FPReturToQCDoc = require('../src/inventory/finishing-printing/retur-to-qc-doc/fp-retur-to-qc-doc');
-    validate.fpReturToQCDoc(new FPReturToQCDoc());
+    validate.finishingPrinting.fpReturToQCDoc(new FPReturToQCDoc());
 });
