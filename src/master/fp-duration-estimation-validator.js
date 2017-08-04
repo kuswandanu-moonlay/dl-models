950ab741d77dd03af58128fd26e51b2018cc2738
indriHutabalian <indri.hutabalian@moonlay.com>
2017-08-04 15:21:20 +0700

    add field code

--- src/master/fp-duration-estimation-validator.js
+++ src/master/fp-duration-estimation-validator.js
@@ -6,6 +6,9 @@ module.exports = function (data) {
     data.should.not.equal(null);
     data.should.instanceOf(Object);
 
+    data.should.have.property('code');
+    data.code.should.instanceOf(String);
+
     data.should.have.property('processTypeId');
     data.processTypeId.should.instanceof(Object);
 
