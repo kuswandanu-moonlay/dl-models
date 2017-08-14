518f6a7a432a19e7aebb883e2294a45e4b483bfb
indriHutabalian <indri.hutabalian@moonlay.com>
2017-08-14 16:41:23 +0700

    fix model invoice note

--- src/garment-purchasing/invoice-note-item-validator.js
+++ src/garment-purchasing/invoice-note-item-validator.js
@@ -1,51 +1,15 @@
 require("should");
-
+var validateInvoiceNoteDeliveryOrderItem = require('./invoice-note-delivery-order-item-validator');
 module.exports = function (data) {
     data.should.have.property('deliveryOrderId');
     data.deliveryOrderId.should.instanceof(Object);
 
     data.should.have.property('deliveryOrderNo');
     data.deliveryOrderNo.should.be.String();
-
-    data.should.have.property('purchaseOrderExternalId');
-    data.purchaseOrderExternalId.should.instanceOf(Object);
-
-    data.should.have.property('purchaseOrderExternalNo');
-    data.purchaseOrderExternalNo.should.be.String();
-
-    data.should.have.property('purchaseOrderId');
-    data.purchaseOrderId.should.instanceOf(Object);
-
-    data.should.have.property('purchaseOrderNo');
-    data.purchaseOrderNo.should.instanceOf(String);
-
-    data.should.have.property('purchaseRequestId');
-    data.purchaseRequestId.should.instanceOf(Object);
-
-    data.should.have.property('purchaseRequestNo');
-    data.purchaseRequestNo.should.instanceOf(String);
-
-    data.should.have.property('productId');
-    data.productId.should.instanceOf(Object);
-
-    data.should.have.property('product');
-    data.product.should.instanceOf(Object);
-
-    data.product.should.have.property('code');
-    data.product.code.should.instanceOf(String);
-
-    data.product.should.have.property('name');
-    data.product.name.should.instanceOf(String);
-
-    data.should.have.property('purchaseOrderQuantity');
-    data.purchaseOrderQuantity.should.instanceOf(Number);
-
-    data.should.have.property('purchaseOrderUom');
-    data.purchaseOrderUom.should.instanceOf(Object);
-
-    data.purchaseOrderUom.should.have.property('unit');
-    data.purchaseOrderUom.unit.should.instanceOf(String);
-
-    data.should.have.property('deliveredQuantity');
-    data.deliveredQuantity.should.instanceOf(Number);
+    
+    data.should.have.property('items');
+    data.items.should.instanceOf(Array);
+    for (var item of data.items) {
+        validateInvoiceNoteDeliveryOrderItem(item);
+    }
 };
