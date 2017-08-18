3390df0ccf782580957ab31afd00b75634bf33b0
indriHutabalian <indri.hutabalian@moonlay.com>
2017-08-18 10:29:28 +0700

    add model unit receipt note

---
+++ src/garment-purchasing/unit-receipt-note-item-validator.js
@@ -0,0 +1,89 @@
+require("should");
+var validateProduct = require('../master/product-validator');
+var validateUom = require('../master/uom-validator');
+var validateCurrency = require('../master/currency-validator');
+
+module.exports = function (data) {
+    data.should.have.property('product');
+    data.product.should.instanceof(Object);
+    // validateProduct(data.product);
+    data.should.have.property('product');
+    data.product.should.instanceof(Object);
+
+    data.product.should.have.property('code');
+    data.product.code.should.instanceOf(String);
+
+    data.product.should.have.property('name');
+    data.product.name.should.instanceOf(String);
+
+    data.should.have.property('deliveredQuantity');
+    data.deliveredQuantity.should.instanceOf(Number);
+
+    data.should.have.property('deliveredUom');
+    data.deliveredUom.should.instanceOf(Object);
+    // validateUom(data.deliveredUom);
+    data.deliveredUom.should.have.property('unit');
+    data.deliveredUom.unit.should.instanceOf(String);
+
+    data.should.have.property('purchaseOrderQuantity');
+    data.purchaseOrderQuantity.should.instanceOf(Number);
+
+    data.should.have.property('pricePerDealUnit');
+    data.pricePerDealUnit.should.instanceOf(Number);
+
+    data.should.have.property('currency');
+    data.currency.should.instanceof(Object);
+    // validateCurrency(data.currency);
+    data.currency.should.have.property('code');
+    data.currency.code.should.instanceOf(String);
+
+    data.currency.should.have.property('symbol');
+    data.currency.symbol.should.instanceOf(String);
+
+    data.currency.should.have.property('rate');
+    data.currency.rate.should.instanceOf(Number);
+
+    data.should.have.property('categoryId');
+    data.categoryId.should.instanceof(Object);
+
+    data.should.have.property('category');
+    data.category.should.instanceof(Object);
+
+    data.should.have.property('purchaseOrderId');
+    data.purchaseOrderId.should.instanceOf(Object);
+
+    data.should.have.property('purchaseOrderNo');
+    data.purchaseOrderNo.should.instanceOf(String);
+
+    data.should.have.property('purchaseRequestId');
+    data.purchaseRequestId.should.instanceOf(Object);
+
+    data.should.have.property('purchaseRequestNo');
+    data.purchaseRequestNo.should.instanceOf(String);
+
+    for (var item of data.purchaseOrder.items) {
+        item.should.have.property('useIncomeTax');
+        item.useIncomeTax.should.instanceof(Boolean);
+
+        item.should.have.property('product');
+        item.product.should.instanceof(Object);
+
+        item.product.should.have.property('code');
+        item.product.code.should.instanceOf(String);
+
+        item.product.should.have.property('name');
+        item.product.name.should.instanceOf(String);
+
+        item.should.have.property('pricePerDealUnit');
+        item.pricePerDealUnit.should.instanceOf(Number);
+
+        item.should.have.property('useIncomeTax');
+        item.useIncomeTax.should.instanceOf(Boolean);
+    }
+
+    data.should.have.property('correction');
+    data.correction.should.instanceOf(Array);
+
+    data.should.have.property('remark');
+    data.remark.should.instanceOf(String);
+};
