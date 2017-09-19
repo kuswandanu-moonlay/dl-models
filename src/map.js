6492410d56bc9d4d4ad5343409f32cb919f4fff1
adityahenanda <henanda.aditya@gmail.com>
2017-09-19 14:20:23 +0700

    rename garment currency collection
    rename garment currency collection

--- src/map.js
+++ src/map.js
@@ -136,7 +136,7 @@ module.exports = {
             GarmentPurchaseCorrection: 'garment-purchase-correction',
             GarmentUnitReceiptNote: 'garment-unit-receipt-notes',
             GarmentInternNote: 'garment-intern-notes',
-            GarmentCurrency:'kurs-currency',
+            GarmentCurrency:'kurs-currencies',
         }
     },
     production: {
