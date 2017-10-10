d870a70cb0cc7c46491bc0112221f908a3ab9594
achensetyawati <achenzetya@gmail.com>
2017-10-10 10:53:06 +0700

    add unit to storage

--- src/master/storage-validator.js
+++ src/master/storage-validator.js
@@ -1,4 +1,5 @@
 require("should");
+var validateUnit=require('./unit-validator');
 
 module.exports = function (data) {
     data.should.not.equal(null);
@@ -9,6 +10,13 @@ module.exports = function (data) {
 
     data.should.have.property('name');
     data.name.should.instanceOf(String);
+    
+    data.should.have.property('unitId');
+    data.unitId.should.instanceOf(Object);
+
+    data.should.have.property('unit');
+    data.unit.should.instanceof(Object);
+    validateUnit(data.unit);
 
     data.should.have.property('description');
     data.description.should.instanceOf(String); 
