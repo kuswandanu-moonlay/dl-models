15a60e08415835e95177473b0999ce7fcd37c35c
Leslie Aula <cristiano.ulo@gmail.com>
2017-10-30 17:18:23 +0700

    add packing receipt items on shipment

---
+++ src/inventory/finishing-printing/shipment-document/fp-shipment-document-packing-receipt-item.js
@@ -0,0 +1,29 @@
+'use strict';
+
+var BaseModel = require('model-toolkit').BaseModel;
+
+module.exports = class ShipmentDocumentPackingReceiptItem extends BaseModel {
+    constructor(source) {
+        super('shipment-document-packing-receipt-item', '1.0.0');
+
+        //Product detail
+        this.productId = "";
+        this.productCode = "";
+        this.productName = "";
+
+        this.designCode = "";
+        this.designNumber = "";
+        this.colorType = "";
+
+        this.uomId = {};
+        this.uomUnit = "";
+        
+        this.quantity = 0;
+        this.length = 0;
+        this.weight = 0;
+
+        this.remark = "";
+
+        this.copy(source);
+    }
+};
