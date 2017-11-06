1a42b02087f800fcd94fdd239214695e4f572c66
indriHutabalian <indri.hutabalian@moonlay.com>
2017-11-06 14:34:27 +0700

    deleten unit

--- src/garment-master-plan/weekly-plan-validator.js
+++ src/garment-master-plan/weekly-plan-validator.js
@@ -1,17 +1,9 @@
 require("should");
-var validateUnit = require('../master/unit-validator');
 var validateItem = require('./weekly-plan-item-validator');
 
 module.exports = function (data) {
     data.should.not.equal(null);
     data.should.instanceOf(Object);
-
-    data.should.have.property('unitId');
-    data.unitId.should.instanceof(Object);
-
-    data.should.have.property('unit');
-    data.unit.should.instanceof(Object);
-    validateUnit(data.unit);
 
     data.should.have.property('year');
     data.year.should.be.instanceOf(Number);
--- src/garment-master-plan/weekly-plan.js
+++ src/garment-master-plan/weekly-plan.js
@@ -7,9 +7,6 @@ var Week = require('./weekly-plan-item');
 module.exports = class WeeklyPlan extends BaseModel {
     constructor(source, type) {
         super(type || 'weekly-plan', '1.0.0');
-
-        this.unitId = {};
-        this.unit = new Unit();
         this.year = 1900;
         this.items = [];
         this.copy(source);
