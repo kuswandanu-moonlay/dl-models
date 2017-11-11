e1f37be8f7fa61d590c521e0e3d0e0a11dd76c99
achensetyawati <achenzetya@gmail.com>
2017-11-11 11:18:44 +0700

    unit test

--- test/garment-master-plan-test.js
+++ test/garment-master-plan-test.js
@@ -4,3 +4,8 @@ it("#01. weekly-plan should valid", function () {
     var WeeklyPlan = require('../src/garment-master-plan/weekly-plan');
     validate.weeklyPlan(new WeeklyPlan());
 });
+
+it("#02. Working Hours Standard should valid", function () {
+    var WorkingHoursStandard = require('../src/garment-master-plan/working-hours-standard');
+    validate.workingHoursStandard(new WorkingHoursStandard());
+});
