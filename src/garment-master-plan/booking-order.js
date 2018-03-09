5e405cf27aa3b6d1ca8628349af940770673869a
kuswandanu <kuswandanu@gmail.com>
2018-03-09 14:22:48 +0700

    Add cancelSisa

--- src/garment-master-plan/booking-order.js
+++ src/garment-master-plan/booking-order.js
@@ -30,6 +30,9 @@ module.exports = class BookingOrder extends BaseModel {
         this.items=[];
         this.canceledItems=[];
 
+        this.canceledBookingOrder = 0;
+        this.canceledDate = null;
+
         this.copy(source);
 
         this.items = (this.items || []).map(item => new BookingItem(item));
