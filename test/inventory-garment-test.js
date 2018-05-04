9ee65c66aabb6b71cfca21ae069ef715b67f7a6c
jackyruslymoonlay <jacky.rusly@moonlay.com>
2018-05-04 11:09:51 +0700

    Inventory Garment

--- test/inventory-garment-test.js
+++ test/inventory-garment-test.js
@@ -2,15 +2,15 @@ var validate = require('../src/validator').garmentInventory;
 
 it("#01. garment-inventory-movement should valid", function () {
     var GarmentInventoryMovement = require('../src/inventory-garment/garment-inventory-movement');
-    validate.inventoryMovement(new GarmentInventoryMovement());
+    validate.garmentInventoryMovement(new GarmentInventoryMovement());
 });
 
 it("#02. garment-inventory-summary should valid", function () {
     var GarmentInventorySummary = require('../src/inventory-garment/garment-inventory-summary');
-    validate.inventorySummary(new GarmentInventorySummary());
+    validate.garmentInventorySummary(new GarmentInventorySummary());
 });
 
 it("#03. garment-inventory document should valid", function () {
     var GarmentInventoryDocument = require('../src/inventory-garment/garment-inventory-document');
-    validate.inventoryDocument(new GarmentInventoryDocument());
+    validate.garmentInventoryDocument(new GarmentInventoryDocument());
 });
